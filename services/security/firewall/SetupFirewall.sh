502bc274d0c1b9b9ca6ed659347935b2d0295970
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-03 09:02:21 +0100

    Update SetupFirewall.sh

--- services/security/firewall/SetupFirewall.sh
+++ services/security/firewall/SetupFirewall.sh
@@ -62,6 +62,10 @@ then
 	/bin/mkdir -p ${HOME}/logs/firewall
 fi
 
+allow_vpc="sshd : `/bin/echo "${VPC_IP_RANGE}" | /usr/bin/awk -F'.' '{print $1,$2,$3.}' | /bin/sed 's/ /./g'`."
+/bin/echo "${allow_vpc}" >> /etc/hosts.allow
+/bin/echo "sshd : ${BUILD_MACHINE_IP}" >> /etc/hosts.allow
+
 #This stream manipulation is required for correct function, please do not remove or comment out
 #exec >${HOME}/logs/firewall/FIREWALL_CONFIGURATION.log
 #exec 2>&1
