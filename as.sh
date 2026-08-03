5fa38480c0c51a286563da3e47a56f264e6ed6e3
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-03 08:20:23 +0100

    Update as.sh

--- as.sh
+++ as.sh
@@ -75,8 +75,6 @@ X11Forwarding no" > /etc/ssh/sshd_config.d/99-hardening.conf
 
 ${HOME}/utilities/processing/RunServiceCommand.sh "ssh" restart
 
-/usr/sbin/sshd -T >  ${HOME}/logs/initialbuild/sshd-initial-settings.log
-
 /bin/sed -i "s/managed=false/managed=true/" /etc/NetworkManager/NetworkManager.conf
 /bin/echo "[keyfile]
 unmanaged-devices=none" > /usr/lib/NetworkManager/conf.d/10-globally-managed-devices.conf
@@ -105,6 +103,7 @@ exec 1>>${HOME}/logs/${out_file}
 err_file="initialbuild/autoscaler-build-err-`/bin/date | /bin/sed 's/ //g'`"
 exec 2>>${HOME}/logs/${err_file}
 
+/usr/sbin/sshd -T >  ${HOME}/logs/initialbuild/sshd-initial-settings.log
 
 /bin/echo "${0} `/bin/date`: Beginning the build of the autoscaler" 
 
