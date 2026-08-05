c971eccf94084b3b8bce52dde42b56e830461518
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-05 08:07:40 +0100

    Exclude archives directory from tar archive

--- utilities/housekeeping/ArchiveErrorLogs.sh
+++ utilities/housekeeping/ArchiveErrorLogs.sh
@@ -2,4 +2,4 @@
 
 archive_date="archive-`/usr/bin/date +%d.%m.%y`"
 
-/bin/tar cvfz ${HOME}/logs/archives/${archive_date}/archive.tar.gz ${HOME}/logs
+/bin/tar -cvfz ${HOME}/logs/archives/${archive_date}/archive.tar.gz --exclude "${HOME}/logs/archives" ${HOME}/logs
