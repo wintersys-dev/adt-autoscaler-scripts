89bbef346eee896d83a9cffc1542c69e40fd12a7
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-05 07:55:32 +0100

    Update DeadOrAliveReverseProxies.sh

--- autoscaler/DeadOrAliveReverseProxies.sh
+++ autoscaler/DeadOrAliveReverseProxies.sh
@@ -20,6 +20,20 @@
 #######################################################################################################
 #set -x
 
+if ( [ ! -d ${HOME}/logs/dead_or_alive_reverse_proxy ] )
+then
+        /bin/mkdir -p ${HOME}/logs/dead_or_alive_reverse_proxy  
+fi
+
+log_file="dead_or_alive_out_`/bin/date | /bin/sed 's/ //g'`"
+err_file="dead_or_alive_err_`/bin/date | /bin/sed 's/ //g'`"
+
+/bin/echo "Log file is at: ${HOME}/logs/dead_or_alive_reverse_proxy/${log_file}"
+/bin/echo "Error file is at: ${HOME}/logs/dead_or_alive_reverse_proxy/${err_file}"
+
+exec 1>>${HOME}/logs/dead_or_alive_reverse_proxy/${log_file}
+exec 2>>${HOME}/logs/dead_or_alive_reverse_proxy/${err_file}
+
 #This function is called whenever the script exits or completes to clean up the monitoring that we have set up
 
 probe_by_curl()
