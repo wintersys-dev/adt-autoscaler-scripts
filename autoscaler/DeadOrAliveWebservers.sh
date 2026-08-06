a1d9543669865f4353a2f75a7d60b4d87ec53221
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-06 09:30:55 +0100

    Remove log file path echo statements
    Removed echo statements for log and error file paths.

--- autoscaler/DeadOrAliveWebservers.sh
+++ autoscaler/DeadOrAliveWebservers.sh
@@ -41,9 +41,6 @@ fi
 log_file="dead_or_alive_out"
 err_file="dead_or_alive_err"
 
-/bin/echo "Log file is at: ${HOME}/logs/dead_or_alive_webserver/${log_file}"
-/bin/echo "Error file is at: ${HOME}/logs/dead_or_alive_webserver/${err_file}"
-
 exec 1>>${HOME}/logs/dead_or_alive_webserver/${log_file}
 exec 2>>${HOME}/logs/dead_or_alive_webserver/${err_file}
 
