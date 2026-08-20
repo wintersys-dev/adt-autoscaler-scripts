93137c275e22df5702824623ff7f06e036da6608
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-08-20 15:53:56 +0100

    Update SetupFirewall.sh

--- services/security/firewall/SetupFirewall.sh
+++ services/security/firewall/SetupFirewall.sh
@@ -21,6 +21,17 @@
 #set -x 
 
 export HOME="`/bin/cat /home/homedir.dat`"
+
+if ( [ ! -d ${HOME}/logs/firewall ] )
+then
+        /bin/mkdir -p ${HOME}/logs/firewall
+fi
+
+out_file="firewall/firewall-out-`/bin/date | /bin/sed 's/ //g'`"
+exec 1>>${HOME}/logs/${out_file}
+err_file="firewall/firewall-err-`/bin/date | /bin/sed 's/ //g'`"
+exec 2>>${HOME}/logs/${err_file}
+
 BUILDOS="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDOS'`"
 SSH_PORT="`${HOME}/utilities/config/ExtractConfigValue.sh 'SSHPORT'`"
 CLOUDHOST="`${HOME}/utilities/config/ExtractConfigValue.sh 'CLOUDHOST'`"
@@ -56,11 +67,6 @@ fi
 if ( [ -f ${HOME}/runtime/FIREWALL-ACTIVE ] )
 then
 	exit
-fi
-
-if ( [ ! -d ${HOME}/logs/firewall ] )
-then
-	/bin/mkdir -p ${HOME}/logs/firewall
 fi
 
 allow_vpc="sshd : `/bin/echo "${VPC_IP_RANGE}" | /usr/bin/awk -F'.' '{print $1,$2,$3.}' | /bin/sed 's/ /./g'`."
