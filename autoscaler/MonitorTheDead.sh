acf8cef1208d12a1094d775b2a79edcfb6f0ff68
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-24 15:03:19 +0100

    Update MonitorTheDead.sh

--- autoscaler/MonitorTheDead.sh
+++ autoscaler/MonitorTheDead.sh
@@ -1,5 +1,13 @@
 
 
+
+#If we are trying to build a webserver before the toolkit has been fully installed, we don't want to do anything, so exit
+if ( [ "`${HOME}/services/datastore/operations/ListFromDatastore.sh "config" "INSTALLED_SUCCESSFULLY"`" = "" ] )
+then
+	exit
+fi
+
+CLOUDHOST="`${HOME}/utilities/config/ExtractConfigValue.sh 'CLOUDHOST'`"
 stalled_webserver_build_ips="`/usr/bin/find ${HOME}/runtime/POTENTIAL_STALLED_BUILD:* -mmin +1 -type f | /usr/bin/awk -F':' '{print $NF}'`"
 
 for stalled_webserver_build_ip in ${stalled_webserver_build_ips}
