6c5d2c12ed779b1bd931fd3086f2a69501414d41
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-24 09:30:32 +0100

    Fix comment typo in ProcessScalingEvents.sh

--- autoscaler/ProcessScalingEvents.sh
+++ autoscaler/ProcessScalingEvents.sh
@@ -4,7 +4,7 @@ if ( [ -f ${HOME}/home/${SERVER_USER}/runtime/scaling/scaling.conf-incoming ] )
 then
 
 #need to read number of webservers that are running for this autoscaler
-#get the number of autoscalers that I expect to be running for this autoscaler
+#get the number of webservers that I expect to be running for this autoscaler
 #shutdown webservers that need to be shutdown
 #build webservers that need to be added
   echo "Scaling event"
