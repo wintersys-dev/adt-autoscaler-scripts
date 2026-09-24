cec01153deabf84cb1fbfbd053dd70a681ccd11c
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-24 09:30:10 +0100

    Update ProcessScalingEvents.sh

--- autoscaler/ProcessScalingEvents.sh
+++ autoscaler/ProcessScalingEvents.sh
@@ -2,5 +2,10 @@
 
 if ( [ -f ${HOME}/home/${SERVER_USER}/runtime/scaling/scaling.conf-incoming ] )
 then
+
+#need to read number of webservers that are running for this autoscaler
+#get the number of autoscalers that I expect to be running for this autoscaler
+#shutdown webservers that need to be shutdown
+#build webservers that need to be added
   echo "Scaling event"
 fi
