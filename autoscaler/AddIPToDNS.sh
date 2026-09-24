f4aab343a7cd568ab89cfc24a8912064f614ff39
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-24 17:35:52 +0100

    Update AddIPToDNS.sh

--- autoscaler/AddIPToDNS.sh
+++ autoscaler/AddIPToDNS.sh
@@ -56,10 +56,10 @@ then
 	#Add the ip address to the DNS provider. Once this is done, the webserver should be online then.
 	zonename="`/bin/echo ${WEBSITE_URL} | /usr/bin/awk -F'.' '{$1=""}1' | /bin/sed 's/^ //g' | /bin/sed 's/ /./g'`"
 	zoneid="`${HOME}/services/dns/GetZoneID.sh "${zonename}" "${DNS_USERNAME}" "${DNS_SECURITY_KEY}" "${DNS_CHOICE}"`"
-	if ( [ "`${HOME}/services/dns/GetRecordID.sh "${zoneid}" "${WEBSITE_URL}" "${ip}" "${DNS_USERNAME}" "${DNS_SECURITY_KEY}" "${DNS_CHOICE}"`" = "" ] )
+	if ( [ "`${HOME}/services/dns/GetRecordID.sh "${zoneid}" "${WEBSITE_URL}" "${ip}" "${DNS_USERNAME}" "${DNS_SECURITY_KEY}" "${DNS_CHOICE}"`" != "" ] )
 	then    
 		${HOME}/services/dns/DeleteRecordByIP.sh "${zoneid}" "${DNS_USERNAME}" "${DNS_SECURITY_KEY}" "${WEBSITE_URL}" "${ip}" "${DNS_CHOICE}"
-		${HOME}/services/dns/AddRecord.sh "${zoneid}" "${DNS_USERNAME}" "${DNS_SECURITY_KEY}" "${WEBSITE_URL}" "${ip}" "${DNS_CHOICE}" 
 	fi
+	${HOME}/services/dns/AddRecord.sh "${zoneid}" "${DNS_USERNAME}" "${DNS_SECURITY_KEY}" "${WEBSITE_URL}" "${ip}" "${DNS_CHOICE}" 
 fi
 
