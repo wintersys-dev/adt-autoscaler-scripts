1c53faf35c830163c33c70ae97d9816473ba5859
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-24 15:12:06 +0100

    Refactor webserver scaling logic in BuildWebserver.sh
    Removed redundant directory checks and file creation for active scaled webservers. Added back the directory checks and file creation at the end of the script.

--- autoscaler/BuildWebserver.sh
+++ autoscaler/BuildWebserver.sh
@@ -139,20 +139,6 @@ else
 	${HOME}/services/datastore/operations/PutToDatastore.sh "config" "${ip}" "webserverpublicips" "no"
 	${HOME}/services/datastore/operations/PutToDatastore.sh "config" "${private_ip}" "webserverips" "no"
 
-	if ( [ ! -d ${HOME}/runtime/scaling/active_scaled_webservers/public_ips ] )
-	then
-		/bin/mkdir -p  ${HOME}/runtime/scaling/active_scaled_webservers/public_ips
-	fi
-
-	/bin/touch ${HOME}/runtime/scaling/active_scaled_webservers/public_ips/${ip}
-
-	if ( [ ! -d  ${HOME}/runtime/scaling/active_scaled_webservers/private_ips ] )
-	then
-		/bin/mkdir -p ${HOME}/runtime/scaling/active_scaled_webservers/private_ips
-	fi
-
-	/bin/touch ${HOME}/runtime/scaling/active_scaled_webservers/private_ips/${private_server_ip}
-
 	#We still need to worry that the build out of the machine might potentially stall for some unknown reason
 	if ( [ ! -f ${HOME}/runtime/POTENTIAL_STALLED_BUILD:${ip} ] )
 	then
@@ -293,6 +279,20 @@ if ( [ -f ${HOME}/runtime/POTENTIAL_STALLED_BUILD:${private_ip} ] )
 then
 	/bin/rm ${HOME}/runtime/POTENTIAL_STALLED_BUILD:${private_ip}
 fi 
+
+if ( [ ! -d ${HOME}/runtime/scaling/active_scaled_webservers/public_ips ] )
+then
+	/bin/mkdir -p  ${HOME}/runtime/scaling/active_scaled_webservers/public_ips
+fi
+
+/bin/touch ${HOME}/runtime/scaling/active_scaled_webservers/public_ips/${ip}
+
+if ( [ ! -d  ${HOME}/runtime/scaling/active_scaled_webservers/private_ips ] )
+then
+	/bin/mkdir -p ${HOME}/runtime/scaling/active_scaled_webservers/private_ips
+fi
+
+/bin/touch ${HOME}/runtime/scaling/active_scaled_webservers/private_ips/${private_server_ip}
 
 #Output how long the build took
 end=`/bin/date +%s`
