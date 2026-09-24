0276fbe839351880b37ad84b64d153212544ee41
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-24 14:55:26 +0100

    Modify MonitorTheDead.sh to check for stalled builds
    Update the script to find stalled builds older than 30 minutes.

--- autoscaler/MonitorTheDead.sh
+++ autoscaler/MonitorTheDead.sh
@@ -1,3 +1,5 @@
 
 
-${HOME}/runtime/POTENTIAL_STALLED_BUILD:${private_ip}
+stalled_builds="`/usr/bin/find ${HOME}/runtime/POTENTIAL_STALLED_BUILD: -mmin +30 -type f`"
+
+#${HOME}/runtime/POTENTIAL_STALLED_BUILD:${private_ip}
