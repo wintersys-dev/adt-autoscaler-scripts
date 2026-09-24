063443a88d47dafe405f6d36ddd93fbcaec219fe
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-24 16:35:36 +0100

    Update MonitorForWebsiteDeath.sh

--- autoscaler/MonitorForWebsiteDeath.sh
+++ autoscaler/MonitorForWebsiteDeath.sh
@@ -2,35 +2,54 @@
 
 #set -x
 
+HOME="`/bin/cat /home/homedir.dat`"
+CLOUDHOST="`${HOME}/utilities/config/ExtractConfigValue.sh 'CLOUDHOST'`"
+
 check_if_webserver_online()
 {
         active_webserver_ip="${1}"
         headfile="`${HOME}/autoscaler/SelectHeadFile.sh`"
+        active_webserver_public_ip="`${HOME}/services/server/GetServerPublicIPAddressByIP.sh ${active_webserver_ip} ${CLOUDHOST}`"
 
         count="0"
         while ( [ "${count}" -lt "5" ] && [ "`${HOME}/utilities/status/CheckWebsiteOnlineStatus.sh ${active_webserver_ip}/${headfile}`" = "failure" ] )
         do
                 count="`/usr/bin/expr ${count} + 1`"
         done
+        online="success"
         if ( [ "${count}" != "0" ] && [ "`${HOME}/utilities/status/CheckWebsiteOnlineStatus.sh ${active_webserver_ip}/${headfile}`" = "failure" ] )
         then
                 active_webserver_public_ip="`${HOME}/services/server/GetServerPublicIPAddressByIP.sh ${active_webserver_ip} ${CLOUDHOST}`"
+                ${HOME}/autoscaler/RemoveIPFromDNS.sh ${active_webserver_public_ip}
                 ${HOME}/services/server/DestroyServer.sh ${active_webserver_public_ip} ${CLOUDHOST}
+
                 if ( [ "`${HOME}/utilities/config/ExtractConfigValue.sh 'DATABASEINSTALLATIONTYPE'`" = "DBaaS" ] && [ "`${HOME}/utilities/config/CheckConfigValue.sh BUILDMACHINEVPC:0`" = "1" ] )
                 then
                         ${HOME}/services/dbaas/AdjustDBaaSFirewall.sh ${public_ip_address}
                 fi
+                online="failure"
+        fi
+
+        if ( [ "${online}" = "success" ] )
+        then
+                dns_ips="`${HOME}/autoscaler/GetDNSIPs.sh`"
+                if ( [ "`/bin/echo ${dns_ips} | /bin/grep ${active_webserver_public_ip}`" = "" ] )
+                then
+                        ${HOME}/autoscaler/AddIPToDNS.sh ${active_webserver_public_ip}
+                fi
         fi
 }
 
-CLOUDHOST="`${HOME}/utilities/config/ExtractConfigValue.sh 'CLOUDHOST'`"
-stalled_webserver_build_ips="`/usr/bin/find ${HOME}/runtime/POTENTIAL_STALLED_BUILD:* -mmin +30 -type f | /usr/bin/awk -F':' '{print $NF}'`"
+if ( [ -f ${HOME}/runtime/POTENTIAL_STALLED_BUILD:* ] )
+then
+        stalled_webserver_build_ips="`/usr/bin/find ${HOME}/runtime/POTENTIAL_STALLED_BUILD:* -mmin +30 -type f | /usr/bin/awk -F':' '{print $NF}'`"
 
-#Any machine that takes longer than 30 minutes to build is considered a stalled build and should be destroyed
-for stalled_webserver_build_ip in ${stalled_webserver_build_ips}
-do
-        ${HOME}/services/server/DestroyServer.sh ${stalled_webserver_build_ip} ${CLOUDHOST}
-done
+        #Any machine that takes longer than 30 minutes to build is considered a stalled build and should be destroyed
+        for stalled_webserver_build_ip in ${stalled_webserver_build_ips}
+        do
+                ${HOME}/services/server/DestroyServer.sh ${stalled_webserver_build_ip} ${CLOUDHOST}
+        done
+fi
 
 active_webserver_ips="`/bin/ls ${HOME}/runtime/scaling/active_scaled_webservers/private_ips`"
 for active_webserver_ip in ${active_webserver_ips}
