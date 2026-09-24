52a8631abd9c02cda47dda79bf17b2bfb020090d
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-24 11:07:06 +0100

    Update BuildWebserver.sh to handle public/private directories
    Create directories for public and private active scaled webservers if they do not exist.

--- autoscaler/BuildWebserver.sh
+++ autoscaler/BuildWebserver.sh
@@ -142,11 +142,15 @@ else
 	${HOME}/services/datastore/operations/PutToDatastore.sh "config" "${ip}" "webserverpublicips" "no"
 	${HOME}/services/datastore/operations/PutToDatastore.sh "config" "${private_ip}" "webserverips" "no"
 
-	if ( [ ! -d ${HOME}/runtime/scaling/active_scaled_webservers ] )
+	if ( [ ! -d ${HOME}/runtime/scaling/active_scaled_webservers/public ] )
 	then
-		/bin/touch ${HOME}/runtime/scaling/active_scaled_webservers/${private_ip}
+		/bin/touch ${HOME}/runtime/scaling/active_scaled_webservers/public/${ip}
 	fi
 
+	if ( [ ! -d ${HOME}/runtime/scaling/active_scaled_webservers/private ] )
+	then
+		/bin/touch ${HOME}/runtime/scaling/active_scaled_webservers/private/${private_ip}
+	fi
 
 	#We still need to worry that the build out of the machine might potentially stall for some unknown reason
 	if ( [ ! -f ${HOME}/runtime/POTENTIAL_STALLED_BUILD:${private_ip} ] )
