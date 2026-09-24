c4a0ae6673a37a2a83b894eb332fd7102ec2fb9b
wintersys-dev <152518070+wintersys-dev@users.noreply.github.com>
2026-09-24 09:44:44 +0100

    Update ProcessScalingEvents.sh

--- autoscaler/ProcessScalingEvents.sh
+++ autoscaler/ProcessScalingEvents.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
-if ( [ -f ${HOME}/home/${SERVER_USER}/runtime/scaling/scaling.conf-incoming ] )
-then
+set -x
 
-#need to read number of webservers that are running for this autoscaler
-#get the number of webservers that I expect to be running for this autoscaler
-#shutdown webservers that need to be shutdown
-#build webservers that need to be added
-  echo "Scaling event"
-fi
+REGION="`${HOME}/utilities/config/ExtractConfigValue.sh 'REGION'`"
+CLOUDHOST="`${HOME}/utilities/config/ExtractConfigValue.sh 'CLOUDHOST'`"
+BUILD_IDENTIFIER="`${HOME}/utilities/config/ExtractConfigValue.sh 'BUILDIDENTIFIER'`"
+webserver_ips="`${HOME}/services/server/GetServerIPAddresses.sh "ws-${REGION}-${BUILD_IDENTIFIER}" ${CLOUDHOST}`"
+autoscaler_no="`/usr/bin/hostname | /usr/bin/awk -F'-' '{print $2}'`"
+
+for ip in ${webserver_ips}
+do
+        webserver_names="`${HOME}/services/server/GetServerName.sh ${ip} ${CLOUDHOST} | /bin/grep -v '\-init\-'`"
+done
+
+no_running_webservers="0"
+for webserver_name in ${webserver_names}
+do
+        if ( [ "`/bin/echo ${webserver_name} | /usr/bin/awk -F'-' '{print $4}'`" = "${autoscaler_no}" ] )
+        then
+                no_running_webservers="`/usr/bin/expr ${no_running_webservers} + 1`"
+        fi
+done
